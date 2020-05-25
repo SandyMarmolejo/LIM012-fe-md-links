665314e26cb1978fb80778c7c8cd5cb728721f06
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-05-25 17:44:31 -0500

    Modificacion de la funcion mdLinks, implementacion de resolve y reject en la promesa

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,80 +1,72 @@
-/* eslint-disable no-console */
-/* eslint-disable max-len */
+const chalk = require('chalk');
 const main = require('./main.js');
 
-
-const mdLinks = (ruta, options) => {
-  // eslint-disable-next-line no-new
-  new Promise((resolve, reject) => {
-    let rutaAbsoluta = '';
-    if (main.esRutaAbsoluta(ruta)) {
-      rutaAbsoluta = ruta;
-    } else {
-      rutaAbsoluta = main.convertirARutaAbsoluta(ruta);
-    }
-    if (main.esRutaValida(rutaAbsoluta)) {
-      if (main.esArchivo(rutaAbsoluta)) {
-        if (main.esArchivoMd(rutaAbsoluta)) {
-          const links = main.obtenerLinks(rutaAbsoluta);
-          if (links.length > 0) {
-            // Si se le pasa la opcion de validar o cuando el programa se ejecuta por default
-            if (options === undefined || options.validate === false) {
-              links.forEach((link) => {
-                // RESOLVE MOSTRAR POR DEFAULT UN ARRAY DE OBJETOS CON 3 PROPIEDADES POR CADA LINK
-                // FILE HREF Y TEXT(TRUNCADO A 50 CARACTERES)
-                console.log(`${link.file} ${link.href} ${link.text}`);
-              });
-            } else if (options.validate === true) {
-              // RESOLVE CON LA OPCION VALIDAR, MOSTRAR UN ARRAY DE OBJETOS CON 5 PROPIEDADES
-              return main.validarLinks(links);
-            }
-          } else {
-            // REJECT
-            console.log('No se encontraron links en este archivo');
+const mdLinks = (ruta, options) => new Promise((resolve, reject) => {
+  let rutaAbsoluta = '';
+  if (main.esRutaAbsoluta(ruta)) {
+    rutaAbsoluta = ruta;
+  } else {
+    rutaAbsoluta = main.convertirARutaAbsoluta(ruta);
+  }
+  if (main.esRutaValida(rutaAbsoluta)) {
+    if (main.esArchivo(rutaAbsoluta)) {
+      if (main.esArchivoMd(rutaAbsoluta)) {
+        const links = main.obtenerLinks(rutaAbsoluta);
+        if (links.length > 0) {
+          if (options === undefined || options.validate === false) {
+            links.forEach((link) => {
+              console.log(`${link.file} ${link.href} ${link.text}`);
+            });
+          } else if (options.validate === true) {
+            main.validarLinks(links);
           }
         } else {
-          // REJECT, MOSTRAR MENSAJE
-          console.log('Tipo de archivo no válido');
+          const error = new Error('No se encontraron links en este archivo');
+          reject(error.message);
         }
       } else {
-        // Navegar directorio en búsqueda de archivos md, retorna la ruta absoluta de los archivos md.
-        // eslint-disable-next-line max-len
-        // Paso como parámetro un array vacío, el resultado acum se guarda en rutaArchivosMdDelDirectorio.
-        const rutaArchivosMdDelDirectorio = main.obtenerArchivosMdDelDirectorio(rutaAbsoluta, []);
-        const linksDelDirectorio = [];
-
-        rutaArchivosMdDelDirectorio.forEach((rutaArchivoMd) => {
-          const links = main.obtenerLinks(rutaArchivoMd);
-          if (links.length > 0) {
-            links.forEach((link) => {
-              linksDelDirectorio.push(link);
-            });
-          }
-        });
-        // RESOLVE, MOSTRAR POR DEFAULT UN ARRAY DE OBJETOS CON 3 PROPIEDADES X CADA LINK
-        // FILE HREF Y TEXT(TRUNCADO A 50 CARACTERES)
-        if (options === undefined || options.validate === false) {
-          linksDelDirectorio.forEach((link) => {
-            console.log(`${link.file} ${link.href} ${link.text}`);
-          });
-        } else if (options.validate === true) {
-          main.validarLinks(linksDelDirectorio);
-        }
-
-        // REJECT, SI NO ENCUENTRA LINKS EN EL DIRECTORIO, MUESTRA MENSAJE
-        if (linksDelDirectorio.length === 0) {
-          console.log('No hay links');
-        }
+        const error = new Error('Tipo de archivo no válido');
+        reject(error.message);
       }
     } else {
-      console.log('La ruta no es válida');
+      // Navegar directorio en búsqueda de archivos md, retorna la ruta absoluta de los archivos md.
+      // Paso como parámetro un array vacío, el resultado acum se guarda en rutaArchivosMdDelDirectorio.
+      const rutaArchivosMdDelDirectorio = main.obtenerArchivosMdDelDirectorio(rutaAbsoluta, []);
+      const linksDelDirectorio = [];
+
+      rutaArchivosMdDelDirectorio.forEach((rutaArchivoMd) => {
+        const links = main.obtenerLinks(rutaArchivoMd);
+        if (links.length > 0) {
+          links.forEach((link) => {
+            linksDelDirectorio.push(link);
+          });
+        }
+      });
+      if (options === undefined || options.validate === false) {
+        linksDelDirectorio.forEach((link) => {
+          console.log(`${link.file} ${link.href} ${link.text}`);
+        });
+      } else if (options.validate === true) {
+        main.validarLinks(linksDelDirectorio);
+      }
+      if (linksDelDirectorio.length === 0) {
+        const error = new Error('No hay links');
+        reject(error.message);
+      }
     }
-  });
-};
+  } else {
+    const error = new Error('La ruta no es válida');
+    reject(error.message);
+  }
+});
 
-//mdLinks('E:\\Laboratoria Sandy\\Proyectos Sandy\\LIM012-fe-md-links\\Dir01Prueba\\PRUEBA02.md');
 
-mdLinks('E:\\Laboratoria Sandy\\Proyectos Sandy\\LIM012-fe-md-links\\Dir01Prueba');
+mdLinks('E:\\Laboratoria Sandy\\Proyectos Sandy\\LIM012-fe-md-links\\Dir01Prueba\\PRUEBA02.md', { validate: false })
+// mdLinks('E:\\Laboratoria Sandy\\Proyectos Sandy\\LIM012-fe-md-links\\Dir01Prueba', { validate: true })
+// mdLinks('E:\\Laboratoria Sandy\\Proyecto Sandy\\LIM012-fe-md-links\\Dir01Prueba')
+  .then(element => console.log(element))
+  .catch(error => console.log(chalk.red(error)));
+
 
 module.exports = {
   mdLinks,
