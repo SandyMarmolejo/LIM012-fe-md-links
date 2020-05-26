f982cf16a2eee483f70220ff4611ffd4e1753a3e
Sandy Marmolejo <sandymarmolejoc@gmail.com>
2020-05-26 14:19:40 -0500

    Adición de mensajes en los argumentos

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const process = require('process');
-const mdLinks = require('./mdLinks.js');
 const chalk = require('chalk');
+const mdLinks = require('./mdLinks.js');
 
 const cli = (ruta, opcionUno, opcionDos) => {
   if ((opcionUno === '--validate' && opcionDos === '--stats') || (opcionUno === '--stats' && opcionDos === '--validate')) {
@@ -44,7 +44,7 @@ const cli = (ruta, opcionUno, opcionDos) => {
         }
       })
       .catch(console.error);
-  } else if (opcionUno == undefined && opcionDos == undefined) {
+  } else if (opcionUno === undefined && opcionDos === undefined) {
     mdLinks.mdLinks(ruta)
       .then((links) => {
         if (links.length === 0) {
@@ -62,20 +62,20 @@ const cli = (ruta, opcionUno, opcionDos) => {
 };
 
 if (process.argv.length <= 2) {
-  console.log(chalk.red('Opción invalida'));
-  console.log(chalk.yellow('Ingrese como primer parámetro una ruta'));
-  console.log('Y además tiene las siguientes opciones');
-  console.log('--validate : ');
-  console.log('--stats : ');
-  console.log('--validate --stats : ');
+  console.log(chalk.red('Opción invalida.'));
+  console.log(chalk.yellow('Ingrese como primer parámetro una ruta.'));
+  console.log('Puedes elegir las siguientes opciones.');
+  console.log('--validate : muestra los links encontrados y si funcionan o no.');
+  console.log('--stats : muestra la cantidad total y únicos de links encontrados.');
+  console.log('--validate --stats : muestra la cantidad total, únicos y rotos de links encontrados.');
 } else if (process.argv.length === 3) {
   if (process.argv[2] === '--help') {
-    console.log(chalk.green('Bienvenidos a mdLinks'));
-    console.log(chalk.yellow('Ingrese como primer parámetro una ruta'));
-    console.log('Y además tiene las siguientes opciones');
-    console.log('--validate : ');
-    console.log('--stats : ');
-    console.log('--validate --stats : ');
+    console.log(chalk.green('Bienvenidos a mdLinks.'));
+    console.log(chalk.yellow('Ingrese como primer parámetro una ruta.'));
+    console.log('Puedes elegir las siguientes opciones.');
+    console.log('--validate : muestra los links encontrados y si funcionan o no.');
+    console.log('--stats : muestra la cantidad total y únicos de links encontrados.');
+    console.log('--validate --stats : muestra la cantidad total, únicos y rotos de links encontrados.');
   } else {
     cli(process.argv[2]);
   }
@@ -84,3 +84,15 @@ if (process.argv.length <= 2) {
 } else if (process.argv.length === 5) {
   cli(process.argv[2], process.argv[3], process.argv[4]);
 }
+
+
+// md-links readme01.md --validate --stats
+
+// console.log(process.argv)
+// [
+//   "cosasdelnode",
+//   "cosasdosdelnode",
+//   "readme01.md",
+//   "--validate",
+//   "-stats"
+// ]
